ab6041ab426e79e951b1606645f776d892d2c749
Hossamhaddad <72374558+Hossamhaddad@users.noreply.github.com>
2022-04-04 11:12:04 +0300

    Update DotButton.jsx

--- src/componenets/DotsButton/DotButton.jsx
+++ src/componenets/DotsButton/DotButton.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Dots } from 'componenets/DotsButton/dotbutton-styles';
-
+/**
+ * Render Dot Button element.
+ *
+ * @return {JSX.element}
+ */
 function DotButton({ onClick }) {
   return <Dots onClick={onClick} />;
 }
